c00b464e8179d9da6513021b58e40f7a7fa98bf9
Albert Chang <albertchanged@gmail.com>
2017-06-23 13:20:27 -0700

    Updated bug movement 10

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ Enemy.prototype.update = function(dt) {
     // return it to the far left to restart cycle
     if (this.x >= 800) {
         this.x = -100;
-        this.y = Math.random() * 100;
+        this.y = Math.random() * 200 + 100; 
     }
     checkCollision(this);
     reachGoal();
